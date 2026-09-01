12c4089a77f37d41772ea92102a285e15c25d993
shot-aka-0607 <49662073+shot-aka-0607@users.noreply.github.com>
2026-09-01 21:27:22 +0900

    Update import path for discord-interactions module

--- _worker.js
+++ _worker.js
@@ -1,4 +1,4 @@
-import { InteractionType, InteractionResponseType, verifyKey } from 'discord-interactions';
+import { InteractionType, InteractionResponseType, verifyKey } from 'https://esm.sh/discord-interactions@4.0.0';
 
 export default {
   async fetch(request, env, ctx) {
